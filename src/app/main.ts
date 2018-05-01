26bb0ef1d62f3063b4b4b966f0cdc58f8e3c1a79
jarog <jgronost@gmail.com>
2018-05-01 21:58:10 +0200

    add index and polyfills

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,5 @@
+import "../pollyfills";
+
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app.module";
 
---
+++ src/pollyfills.ts
@@ -0,0 +1,3 @@
+import "core-js/es6";
+import "core-js/es7/rflect";
+require("zone.js/dist/zone");
